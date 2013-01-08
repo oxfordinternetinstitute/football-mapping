8124480f0d682e6b9e28b2f504c7948ee899b5f8
computermacgyver <computermacgyver@gmail.com>
2013-01-08 17:19:21 +0900

    implement switching between rilvaries

--- js/main.js
+++ js/main.js
@@ -1,25 +1,69 @@
+"use strict";
 $(document).ready(function() {
+
+		//global vars
+		var current_rivalry = 0; // this is what we will change with our drop down select box
+		var team1_statistic,team2_statistic,team1,team2;
+		var colorMin,colorMax,geojson;
 
 		$('#controlButton').click(function () {
 			 $("#controlwrapper").animate({marginBottom: 0}, 400);
 		 });
-
-			var current_rivalry = 0; // this is what we will change with our drop down select box
+		 
+		 
+		 //Build list of rivalries
+		 var list = $("#rivalryList");
+		 for (var i=0; i<rivalries.length; i++) {
+		 	var r=rivalries[i];
+		 	var li = $("<li/>");
+		 	 $("<a/>",{
+		 		"href":"#riv"+i,
+		 		"title":r.rivalry_info,
+		 		"text": r.rivalry_info,
+		 		"class": "rivalryLink"/*,
+		 		"data-index":i*/
+		 		}).appendTo(li);
+		 	li.appendTo(list);
+		 	//$("<li/>").text(r.rivalry_info).appendTo(list);
+		 }		 
+		 
+		 $("a.rivalryLink").click(function(event) {
+			//console.log(event.srcElement.hash);
+		 	$("#controlwrapper").animate({marginBottom: "-320px"}, 400);//Hide bottom panel
+		 	showData(event.srcElement.hash);
+		 });
+		 
+		 
+		//Build map
+		var map = L.map('map').setView([54.6342, -5.2], 6);
+		L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
+			key: 'BC9A493B41014CAABB98F0471D759707'
+			}).addTo(map);
+		 
+		/*geojson = L.geoJson(uk_post_districts, {
+				//style: style,
+				onEachFeature: onEachFeature
+			}).addTo(map);*/
+			
+		function showData(what) {
+			current_rivalry = what.substr(4);
+			console.log(current_rivalry);
+			//current_rivalry = 0; // this is what we will change with our drop down select box
 			var team1_variable = rivalries[current_rivalry]['teams'][0]['variable'];
 			var team2_variable = rivalries[current_rivalry]['teams'][1]['variable'];
-			
-			var team1_statistic = team1_variable;//+'_norm';
-			var team2_statistic = team2_variable;//+'_norm';
+		
+			team1_statistic = team1_variable;//+'_norm';
+			team2_statistic = team2_variable;//+'_norm';
 			var value1,value2;
-			
-			var colorMin=1,colorMax=0;
+		
+			//var colorMin=1,colorMax=0;
 			var colorDist=[];
 			for (var i=0; i<uk_post_districts.features.length; i++){
 				var postcode = uk_post_districts.features[i].properties["post_4"];
-				var team1 = teamData[postcode][team1_statistic];
-				var team2 = teamData[postcode][team2_statistic];
-				if (team1+team2==0) continue;
-				var n = team1/(team1+team2);
+				var t1 = teamData[postcode][team1_statistic];
+				var t2 = teamData[postcode][team2_statistic];
+				if (t1+t2==0) continue;
+				var n = t1/(t1+t2);
 				/*if (n<colorMin) {
 					colorMin=n;
 				} else if (n>colorMax) {
@@ -34,21 +78,26 @@ $(document).ready(function() {
 			colorMax=colorDist[colorDist.length-1];
 			//console.log(colorMin);
 			//console.log(colorMax);
-			
-			
+		
+		
 			// this will be refactored into some sort of changeRivalry() function
 			$('#team1logo').attr("src", rivalries[current_rivalry]['teams'][0]['crest']);
 			$('#team1name').text(rivalries[current_rivalry]['teams'][0]['name']);
-			
+		
 			$('#team2logo').attr("src", rivalries[current_rivalry]['teams'][1]['crest']);		
 			$('#team2name').text(rivalries[current_rivalry]['teams'][1]['name']);
+			if (geojson) {
+				geojson.setStyle(style);
+			} else {//first time
+				geojson = L.geoJson(uk_post_districts, {
+					style: style,
+					onEachFeature: onEachFeature
+				}).addTo(map);
+			}
+		}//End show data
+		showData("#riv0");
 					
-			var map = L.map('map').setView([54.6342, -5.2], 6);
-
-
-			L.tileLayer('http://{s}.tile.cloudmade.com/{key}/22677/256/{z}/{x}/{y}.png', {
-				key: 'BC9A493B41014CAABB98F0471D759707'
-			}).addTo(map);
+			
 
 			// control that shows state info on hover
 
@@ -119,7 +168,7 @@ $(document).ready(function() {
 				//	rivalries[current_rivalry]['teams'][1]['color'],value2);
 				var col = value1/(value1+value2);
 				col = (col-colorMin)*(1/(colorMax-colorMin));
-				polygoncolor = normBlend(rivalries[current_rivalry]['teams'][0]['color'],rivalries[current_rivalry]['teams'][1]['color'],col);
+				var polygoncolor = normBlend(rivalries[current_rivalry]['teams'][0]['color'],rivalries[current_rivalry]['teams'][1]['color'],col);
 
 				return {
 					weight: 0.5,
@@ -139,71 +188,5 @@ $(document).ready(function() {
 						});
 					}
 
-			geojson = L.geoJson(uk_post_districts, {
-				style: style,
-				onEachFeature: onEachFeature
-			}).addTo(map);
 			
-			
-			//Color blending functions
-			function normBlend(a,b,weightA) {
-				a=hexToRgb(a);
-				b=hexToRgb(b);
-				
-				weightA=(weightA>1)?1:weightA;
-				weightA=(weightA<0)?0:weightA;
-
-				var blend = {};
-				blend.r=a.r*weightA+b.r*(1-weightA);
-				blend.g=a.g*weightA+b.g*(1-weightA);
-				blend.b=a.b*weightA+b.b*(1-weightA);
-				//console.log(blend);
-
-				var hex = rgbToHex(blend.r,blend.g,blend.b);
-				//console.log(hex);
-
-				return hex;
-			}
-			
-			function blend(a,weightA,b,weightB) {
-				return normBlend(a,b,weightA/(weightA+weightB));
-				/*a=hexToRgb(a);
-				b=hexToRgb(b);
-
-				//console.log(a);
-				//console.log(b);
-	
-				var weightSum = weightA+weightB;
-	
-				var blend = {}
-				blend.r=(a.r*weightA+b.r*weightB)/weightSum;
-				blend.g=(a.g*weightA+b.g*weightB)/weightSum;
-				blend.b=(a.b*weightA+b.b*weightB)/weightSum;
-				//console.log(blend);
-
-				var hex = rgbToHex(blend.r,blend.g,blend.b);
-				//console.log(hex);
-
-				return hex;*/
-
-			}
-
-			function hexToRgb(hex) {
-				var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-				return result ? {
-					r: parseInt(result[1], 16),
-					g: parseInt(result[2], 16),
-					b: parseInt(result[3], 16)
-				} : null;
-			}
-
-			function componentToHex(c) {
-				c=Math.round(c);
-				var hex = c.toString(16);
-				return hex.length == 1 ? "0" + hex : hex;
-			}
-
-			function rgbToHex(r, g, b) {
-				return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-			}
 });
